73a39ba161f5ab05422d7f06711a165d45ebaa99
Nico2125-hub <83259750+Nico2125-hub@users.noreply.github.com>
2021-06-04 16:34:45 -0500

    MantenedorCurso

---
+++ Capa Presentacion/Mantenedor/Mantenedor/MantenedorCurso.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Mantenedor
+{
+    public partial class MantenedorCurso : Form
+    {
+        public MantenedorCurso()
+        {
+            InitializeComponent();
+        }
+    }
+}
