e78a75a075c8209d69a1eb6d1f9d4a882f732e0b
Milton Mariños <milcesar.mp98@hotmail.com>
2021-06-23 15:48:28 -0500

    add: entidad tutor

--- Capa Entidad/Tutor.cs
+++ Capa Entidad/Tutor.cs
@@ -9,5 +9,16 @@ namespace CapaEntidad
     public class Tutor
     {
         public int Id { get; set; }
+        public string Nombres { get; set; }
+        public string ApPaterno { get; set; }
+        public string ApMaterno { get; set; }
+        public char Sexo { get; set; }
+        public DateTime FecNacimiento { get; set; }
+        public string Direccion { get; set; }
+        public string Email { get; set; }
+        public string Ocupacion { get; set; }
+        public string NvAcademico { get; set; }
+
+
     }
 }
