d01e4f3d1a517afca1d9814667f19f9b6f796914
Milton Mariños <milcesar.mp98@hotmail.com>
2021-06-23 10:27:06 -0500

    se realizo la conexion con la db

--- Capa Datos/Conexion.cs
+++ Capa Datos/Conexion.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +9,21 @@ namespace Capa_Datos
 {
     class Conexion
     {
+        private static readonly Conexion _instancia = new Conexion();
+        #region singleton
+        public static Conexion Instancia
+        {
+            get { return Conexion._instancia; }
+        }
+        #endregion singleton
+
+        public SqlConnection Conectar()
+        {
+            SqlConnection cn = new SqlConnection
+            {
+                ConnectionString = "Data Source=.; Initial Catalog=SistemaVenta; Integrated Security=true"
+            };
+            return cn;
+        }
     }
 }
