d02c7591a5cf6d8d237a01ad5c9da948ac2939ba
Milton Mariños <milcesar.mp98@hotmail.com>
2021-06-23 11:05:44 -0500

    add: logica estudiante

--- Capa Logica/LgcEstudiante.cs
+++ Capa Logica/LgcEstudiante.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using CapaDatos;
+using CapaEntidad;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,8 +8,13 @@ using System.Threading.Tasks;
 
 namespace CapaLogica
 {
-    class LgcEstudiante
+    public class LgcEstudiante
     {
+        public static LgcEstudiante Instancia { get; } = new LgcEstudiante();
 
+        public List<Estudiante> ListarEstudiantes() 
+        {
+            return SqlEstudiante.Instancia.ListarEstudiantes();
+        }
     }
 }
