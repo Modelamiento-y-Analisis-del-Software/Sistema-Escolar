e29a44670367e3bc5fd93e1e9b22f412754e969e
Milton Mariños <milcesar.mp98@hotmail.com>
2021-06-23 16:22:25 -0500

    add: logica tutor

---
+++ Capa Logica/LgcTutor.cs
@@ -0,0 +1,20 @@
+﻿using CapaDatos;
+using CapaEntidad;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaLogica
+{
+    public class LgcTutor
+    {
+        public static LgcTutor Instancia { get; } = new LgcTutor();
+
+        public List<Tutor> ListarTutores()
+        {
+            return SqlTutor.Instancia.ListarTutores();
+        }
+    }
+}
