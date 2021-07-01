c8b6999483f5b89c43f713688d77dc4b54c16be8
Milton Mariños <milcesar.mp98@hotmail.com>
2021-06-30 23:36:13 -0500

    add: entidad calificacion

---
+++ Capa Entidad/Calificacion.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaEntidad
+{
+    public class Calificacion
+    {
+        public int Id { get; set; }
+        public Tema EntTema { get; set; }
+        public int Nota { get; set; }
+    }
+}
